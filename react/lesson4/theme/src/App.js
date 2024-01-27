0516882b79f15250d36c32698771ba5e8f0330a8
BJORN-VINTHER <97097933+BJORN-VINTHER@users.noreply.github.com>
2024-01-28 00:20:08 +0100

    tmp commit

--- react/lesson4/theme/src/App.js
+++ react/lesson4/theme/src/App.js
@@ -5,13 +5,10 @@ export default function App() {
   return (
     <>
       <div className="title-container">
-        <h1>Welcome to ThemeHub!</h1>
+        <h1>Welcome to ThemeHub</h1>
         <p>
           Please browse our themes and choose the one you like the most.
         </p>
-        <div>
-          <ThemedButton label="Browse All Themes" onClick={() => console.log("Clicked on Browse All Themes")} />
-        </div>
       </div>
 
 
@@ -19,7 +16,7 @@ export default function App() {
       <p>
         You never go wrong with a classic light theme. It's easy on the eyes and it's the default theme for most applications.
       </p>
-      <ThemedButton label="Get Theme" onClick={() => console.log("Got light theme")} />
+      <ThemedButton label="Use Light Theme" onClick={() => console.log("Got light theme")} />
       <ThemedButton label="Read more..." secondary onClick={() => console.log("Read more about light theme")} />
 
 
@@ -28,7 +25,7 @@ export default function App() {
       <p>
         Dark theme is a great choice for people who like to work late at night. It's easy on the eyes and it's the default theme for most applications.
       </p>
-      <ThemedButton label="Get Theme" onClick={() => console.log("Got dark theme")} />
+      <ThemedButton label="Use Dark Theme" onClick={() => console.log("Got dark theme")} />
       <ThemedButton label="Read more..." secondary onClick={() => console.log("Read more about dark theme")} />
 
 
@@ -37,7 +34,7 @@ export default function App() {
       <p>
         Do you want to have a cat theme? We have a cat theme for you. This theme is for people who like cats.
       </p>
-      <ThemedButton label="Get Theme" onClick={() => console.log("Got cat theme")} />
+      <ThemedButton label="Use Cat Theme" onClick={() => console.log("Got cat theme")} />
       <ThemedButton label="Read more..." secondary onClick={() => console.log("Read more about cat theme")} />
     </>
   );
--- react/lesson4/theme/src/components/ThemedHeader.js
+++ react/lesson4/theme/src/components/ThemedHeader.js
@@ -1,7 +1,8 @@
 import "./ThemedHeader.css";
 
-export default function ThemedHeader({ title }) {
+export default function ThemedHeader({ title, theme }) {
+    const classes = `themed-header ${theme}`;
     return (
-        <h2 className="light">{title}</h2>
+        <h2 className={classes}>{title}</h2>
     )
 }
